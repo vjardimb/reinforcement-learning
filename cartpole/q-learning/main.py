8c1f41b476f1f3e360cda9b233d09b92d31be4c8
vjardimb <vitorjardimbueno@hotmail.com>
2023-02-26 16:06:12 +0100

    corrections

--- cartpole/q-learning/main.py
+++ cartpole/q-learning/main.py
@@ -1,24 +1,27 @@
 import gym
 import numpy as np
 
-
 from utils import *
 
-NO_BINS = [6, 12]
-LIMITS = [
-    [-4.19, -4.19],
-    [-np.radians(50), -np.radians(50)]
-]
 
 def train_model():
     env = gym.make("CartPole-v1", render_mode="human")
     env.reset()
 
-    Q_table = np.zeros((NO_BINS[0], NO_BINS[1], 2))
+    NO_BINS = [1, 1, 6, 3]
+    LIMITS = list(zip(env.observation_space.low, env.observation_space.high))
+    LIMITS[1] = (-0.5, 0.5)
+    LIMITS[3] = (-math.radians(50), math.radians(50))
 
-    for episode in range(10000):
+
+    no_episodes = 180
+
+    Q_table = np.zeros(NO_BINS + [2])
+
+    for episode in range(no_episodes):
         obs, info = env.reset()
-        current_state = discretize_state(obs[2:], LIMITS, NO_BINS)
+        current_state = discretize_state(obs, LIMITS, NO_BINS)
+        print(current_state)
 
         done = False
         t = 0
@@ -26,13 +29,13 @@ def train_model():
         while not done:
             env.render()
 
-            if np.random.random() < exploration_rate(episode):
+            if np.random.random() > exploration_rate(episode):
                 action = get_opt_action(Q_table, current_state)
             else:
                 action = env.action_space.sample()
 
             obs, reward, done, truncated, info = env.step(action)
-            new_state = discretize_state(obs[2:], LIMITS, NO_BINS)
+            new_state = discretize_state(obs, LIMITS, NO_BINS)
 
             lr = get_l_rate(episode)
             Q_table = update_q_table(Q_table, new_state, current_state, action, reward, lr)
@@ -40,23 +43,29 @@ def train_model():
             t += 1
             score += reward
             current_state = new_state
+
             if done:
-                print(f"done in {t} steps - episode no {episode} - score: {score}")
-                break
+                print(f"Episode no {episode} - Score: {score}")
 
     np.save("Q_table.npy", Q_table)
     env.close()
     return
 
+
 def use_trained_model():
     env = gym.make("CartPole-v1", render_mode="human")
     env.reset()
+
+    NO_BINS = [1, 1, 6, 3]
+    LIMITS = list(zip(env.observation_space.low, env.observation_space.high))
+    LIMITS[1] = (-0.5, 0.5)
+    LIMITS[3] = (-math.radians(50), math.radians(50))
 
     Q_table = np.load("Q_table.npy")
 
     for i in range(10):
         obs, info = env.reset()
-        state = discretize_state(obs[2:], LIMITS, NO_BINS)
+        state = discretize_state(obs, LIMITS, NO_BINS)
 
         done = False
         while not done:
@@ -65,13 +74,14 @@ def use_trained_model():
             action = get_opt_action(Q_table, state)
 
             obs, reward, done, truncated, info = env.step(action)
-            state = discretize_state(obs[2:], LIMITS, NO_BINS)
+            state = discretize_state(obs, LIMITS, NO_BINS)
 
     env.close()
     return
 
 
 if __name__ == "__main__":
+    # train_model()
     try:
         use_trained_model()
     except FileNotFoundError:
--- cartpole/q-learning/utils.py
+++ cartpole/q-learning/utils.py
@@ -17,22 +17,21 @@ def discretize_state(state, limits, no_bins):
 			bin_index = no_bins[i] - 1
 		else:
 			scale_portion = (coord - limits[i][0])/total_range
-			bin_index = np.floor(no_bins*scale_portion)
+			bin_index = int(np.floor(no_bins[i]*scale_portion))
 
-		# disc_state[i] = bin_index*(total_range/no_bins[i]) + limits[i][0]
 		disc_state[i] = int(bin_index)
 
 	return tuple(disc_state)
 
 
-def update_q_table(Q_table, new_state, current_state, action, reward, lr, disc_factor=0.95):
+def update_q_table(Q_table, new_state, current_state, action, reward, lr, disc_factor=0.99):
 	"""
 	Updates Q table based on rewards using bellman's equation.
 	"""
-	learned_Q_val = reward + disc_factor*np.max(Q_table[new_state[0], new_state[1]])
-	old_Q_val = Q_table[current_state[0], current_state[1], action]
+	learned_Q_val = reward + disc_factor*np.max(Q_table[new_state])
+	old_Q_val = Q_table[current_state][action]
 
-	Q_table[current_state[0], current_state[1], action] = (1 - lr)*old_Q_val + lr*learned_Q_val
+	Q_table[current_state][action] = (1 - lr)*old_Q_val + lr*learned_Q_val
 
 	return Q_table
 
@@ -41,7 +40,7 @@ def get_opt_action(Q_table, state):
 	"""
 	Chooses action based on greedy policy i.e. looks at the action the the highest expected reward.
 	"""
-	return int(np.argmax(Q_table[state[0], state[1]]))
+	return int(np.argmax(Q_table[state]))
 
 
 def get_l_rate(episode, min_rate=0.01):
